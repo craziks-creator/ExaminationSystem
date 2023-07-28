51c8f36f49846cdfb6abac7c25039de9f414acdf
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-07-28 11:24:43 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Route::group(['prefix'=>'admin', 'middleware'=>['auth','admin_auth']], function (){
